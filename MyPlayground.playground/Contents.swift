d2b5089e9ffa21edff69c11c50dd3689af80c27c
camileancines <camileancines@gmail.com>
2020-09-06 21:22:14 -0300

    adição de comentários no código

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,16 +2,21 @@ import UIKit
 
 class MailTextViewController: UIViewController {
     
+    //MARK: - Componente da tela: TextView onde o texto do e-mail será digitado.
     var emailContentTextView: UITextView!
     
+    //MARK: - Componentes de texto:
     var textFont: UIFont!
     var attributedText: NSMutableAttributedString!
     var setTextAttribute: [NSAttributedString.Key: Any]!
     
+    //MARK: - Funções para atribuição da ação de  cada botão.
+    
+    //MARK: - Ação do botão que atribui 'negrito' ao texto selecionado.
     func setBoldTextButtonAction(_ sender: UIButton) {
         
-        attributedText = NSMutableAttributedString(attributedString: emailContentTextView.attributedText)
         textFont = UIFont.boldSystemFont(ofSize: 17)
+        attributedText = NSMutableAttributedString(attributedString: emailContentTextView.attributedText)
         setTextAttribute = [NSAttributedString.Key.font: textFont as Any]
         attributedText.addAttributes(setTextAttribute, range: emailContentTextView.selectedRange)
         
@@ -19,10 +24,11 @@ class MailTextViewController: UIViewController {
         
     }
     
+    //MARK: - Ação do botão que atribui 'itálico' ao texto selecionado.
     func setItalicTextButtonAction(_ sender: UIButton) {
         
-        attributedText = NSMutableAttributedString(attributedString: emailContentTextView.attributedText)
         textFont = UIFont.italicSystemFont(ofSize: 17)
+        attributedText = NSMutableAttributedString(attributedString: emailContentTextView.attributedText)
         setTextAttribute = [NSAttributedString.Key.font: textFont as Any]
         attributedText.addAttributes(setTextAttribute, range: emailContentTextView.selectedRange)
         
@@ -30,6 +36,7 @@ class MailTextViewController: UIViewController {
         
     }
     
+    //MARK: - Ação do botão que atribui 'sublinhado' ao texto selecionado.
     func setUnderlineTextButtonAction(_ sender: UIButton) {
         
         attributedText = NSMutableAttributedString(attributedString: emailContentTextView.attributedText)
